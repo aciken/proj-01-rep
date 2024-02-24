7b418df1d68055ec2db2c88cff8e0989d34af4a5
aciken <adrianmarton2006@gmail.com>
2024-02-24 16:57:43 +0100

    front again

--- FrontEnd/src/YoutubeUpload.jsx
+++ FrontEnd/src/YoutubeUpload.jsx
@@ -324,10 +324,15 @@ const uploadFile = (file, fileType) =>{
       // For instance, get the download URL: https://firebasestorage.googleapis.com/...
       getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
         console.log('File available at', downloadURL);
-        setInputs(downloadURL)
+        setInputs((prev) =>{
+            return {
+            ...prev,
+             [fileType]: downloadURL,
+            }}
+            );
+      });
     }
-      );
-});
+  );
   
 
 }
