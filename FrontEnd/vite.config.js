edcadf88741530e0fbf35cbad754460d4ddfa00b
aciken <adrianmarton2006@gmail.com>
2024-04-28 20:17:32 +0200

    png2

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd',
-          style: (name) => `antd/es/${name}/style/index.css`,
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    assetsInlineLimit: 0,
+    assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg', '**/*.gif'],
+  },
 })
