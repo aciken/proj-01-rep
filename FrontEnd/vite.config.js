f4903f6db0a0ed66884211d37bd28f613234f24c
aciken <adrianmarton2006@gmail.com>
2024-04-28 20:09:36 +0200

    vite-con

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style/index.css`,
+        },
+      ],
+    }),
+  ],
 })
